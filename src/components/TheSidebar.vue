e5cc3da7eb1f73e90cdf806adaedc79480cb6bf0
Jun-Hong Cheng <qwe854896@gmail.com>
2023-07-08 02:30:16 +0800

    feat: set up simple layout

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+</script>
+
+<template>
+  <aside class="sidebar" :class="{ active: isOpen }">
+    <div class="sidebar-info">
+      <figure class="avatar-box">
+        <img src="@/assets/avatar.jpg" alt="Photo" width="80" />
+      </figure>
+
+      <div class="info-content">
+        <h1 class="name" title="Jun-Hong Cheng">Jun-Hong Cheng</h1>
+      </div>
+    </div>
+  </aside>
+</template>
